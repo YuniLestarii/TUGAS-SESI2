a1514813a0bb2cf5bdb9f299257593d7a61aab60
YuniLestarii <123483879+YuniLestarii@users.noreply.github.com>
2023-10-03 09:53:22 +0700

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,9 @@
+let firstName = "Yuni ";
+let lastName = "Lestari ";
+let age = 21;
+let isMarried = false
+
+console.log(" Nama depan : "+ firstName)
+console.log(" Nama belakang : "+ lastName)
+console.log(" Usia : "+ age)
+console.log(" Menikah : "+ isMarried)
